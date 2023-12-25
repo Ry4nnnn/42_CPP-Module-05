d184ef5cc21857f4177697dd2414ab3291a8ced2
Ryan <weijunlimm@gmail.com>
2023-12-25 18:04:34 +0800

    first commit

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -4,4 +4,11 @@ Bureaucrat::Bureaucrat() : _name("Default") {
 	this->_grade = 0;
 }
 
-Bureaucrat::Bureaucrat(std::string _name)
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name) {
+	if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	this->_grade = grade;
+}
+
